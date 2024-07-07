f5a9c8d907de5017b0618424b02271f3342bfc55
Henrietta <hetty8004@gmail.com>
2024-07-07 22:42:05 +0200

    login and home page created

---
+++ morereaders/src/pages/Homepage/Home.jsx
@@ -0,0 +1,13 @@
+import Posts from "../../components/posts/Posts";
+import "./homepage.scss";
+
+const Home = () => {
+  return (
+
+    <div className="home">
+      <Posts/>
+    </div>
+  )
+}
+
+export default Home
---
+++ morereaders/src/pages/login/Login.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
+
+const Login = () => {
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    login();
+  };
+
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button onClick={handleLogin}>Login</button>
+          </form>
+          <span>Don't have an account?
+          <Link to="/register">Signup</Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
