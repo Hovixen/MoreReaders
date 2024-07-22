d5a4d145370f77352117c3d5adcbce8d3465636c
Henrietta <hetty8004@gmail.com>
2024-07-22 16:34:16 +0200

    more updates

--- morereaders/src/components/library/Library.jsx
+++ morereaders/src/components/library/Library.jsx
@@ -5,7 +5,7 @@ import "./library.scss";
 const Library = () => {
     return (
         <div className='holder-center flex flex-c text-center text-white'>
-            <h2 className='search-title text-capitalize'>Find your dream books.</h2>
+            <h2 className='search-title text-capitalize text-white'>Find your dream books.</h2>
             <SearchForm />
         </div>
     )
